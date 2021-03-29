8cbeb19db0719b4057724e2a179c57be26a4e73c
arnell0 <mailtojonsson@gmail.com>
2021-03-29 18:01:04 +0200

    Created function for fetching current weather data

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 A simple page for viewing the weather.
 
-[Homepage](arnell0.github.io)
+## Todo
+
+- <s>create function for fetching cyrrent weather data from freemium API</s>
+  API used https://rapidapi.com/community/api/open-weather-map
+  notes: respones looked weird at first glance, because they use different units then what we "non science people" asume. Such as Kelvin instead of Celsius for temperatur.
+  - convert units
+
+<!-- https://dev.to/yuribenjamin/how-to-deploy-react-app-in-github-pages-2a1f -->
--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+import {exampleData} from './getData'
 
-function App() {
+export default function App() {
+  const [data, setData] = useState(exampleData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={() => getData()}>
+      get &nbsp; <div className="App-color">weather</div>
+      <>{JSON.stringify(data)}</>
     </div>
-  );
-}
+  )
 
-export default App;
+  function getData () {
+    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Uppsala%2Cse&lang=en&units=%22metric%22", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "5a380c6890msh9dc760c861baa4ap12673djsnb7a8195b3c4d",
+        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+        setData(response)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }
+}
